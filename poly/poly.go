ee1d795e6ea4720831d2ba1f71e6a95e3f128011
asmith1024 <asmith1024@localhost.localdomain>
2019-01-07 21:09:42 +1100

    Use correct nomenclature in comment for poly.Y

--- poly/poly.go
+++ poly/poly.go
@@ -3,7 +3,7 @@ package poly
 
 import "math"
 
-// Y calculates the value of a polynomial given a point on the x-axis and a slice of coefficients of degree equal to their index values.
+// Y calculates the value of a polynomial given a point on the x-axis and a slice of coefficients. The degree of the polynomial is the slice high index.
 func Y(x float64, coeffs []float64) float64 {
 	if len(coeffs) == 0 {
 		return 0
