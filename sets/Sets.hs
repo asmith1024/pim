ca399861de65305795d43bb38ae1f89ea692aad1
Andrew Smnith <asmith1024@gmail.com>
2019-03-11 19:14:50 +1100

    developing Haskell approach

--- sets/Sets.hs
+++ sets/Sets.hs
@@ -28,12 +28,32 @@ import Data.List (foldl', sortBy)
 allocatePreferences :: [[Int]] -> [[Int]] -> [(Int, Int)]
 allocatePreferences _ _ = []
 
+-- returns a pair, the first element of identifes a member of the other set, the second its corresponding preference ordinal 
 indexPreferences :: [Int] -> [(Int, Int)]
-indexPreferences = zipWith (\x y -> (x,y)) [1..]
+indexPreferences ps = zipWith (\x y -> (x,y)) ps [1..]
 
---mutualPreferences :: [[Int]] -> [[Int]] -> [((Int, Int), Int)]
---mutualPreferences a b = [((), ) | ]
+indexEverything :: [[Int]] -> [(Int, [(Int, Int)])]
+indexEverything = zipWith(\x y -> (x, indexPreferences y)) [1..]
+
+mutualPreferences :: [[Int]] -> [[Int]] -> [((Int, Int), Int)]
+mutualPreferences as bs = preferenceProduct (indexEverything as) (indexEverything bs)
+
+-- gets the cartesion product of all possible pairings
+preferenceProduct :: [(Int, [(Int, Int)])] -> [(Int, [(Int, Int)])] -> [((Int, Int), Int)]
+preferenceProduct as bs = [((fst x, fst y), findProduct x y) | x <- as, y <- bs]
+
+findProduct :: (Int, [(Int, Int)]) -> (Int, [(Int, Int)]) -> Int
+findProduct as bs = preference (fst bs) (snd as) * preference (fst as) (snd bs)
+
+preference :: Int -> [(Int, Int)] -> Int
+preference i = snd . head . filter (\v -> fst v == i)
 
 areAllocationsStable :: [[Int]] -> [[Int]] -> [(Int, Int)] -> Bool
 areAllocationsStable _ _ _ = False
 
+-- need to first calculate all possible pairings
+-- then calculate the score for each
+-- then return the lowest
+
+-- list of pairs
+-- GLOBAL list of preferences indexed by pair
